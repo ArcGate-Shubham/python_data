32414ab1c12daf74da893b724c440014873ab7d3
stonex <stonex@arcgate2213.arcgate.in>
2022-05-19 17:09:44 +0530

    second time

--- tuple1.py
+++ tuple1.py
@@ -1,8 +1,9 @@
 course =("java","python","Dotnet","c/c++","angular")
 print(course)
-name=input("Enter element name")
+name=input("Enter element name :")
 for x in range(len(course)):
     if(name == course[x]):
-       print("Index are",x) 
+       print("Index are",x)
+       break
     else:
         print("value is not present in this list")
