cecffe0bd27587ff1dafaf68b1384a88aa773e7d
Shubham <shubhamvijayvargiya@arcgate.com>
2022-05-30 12:39:50 +0530

    commit for dictionary

---
+++ dictionary/dict2.py
@@ -0,0 +1,7 @@
+zudio = {'men'   : 'shirts',
+         'women' : 't-shirt',
+         'kids'  : 'pants'
+        }
+
+for x,y in zudio.items():
+    print(x,":",y)
---
+++ dictionary/dict3.py
@@ -0,0 +1,11 @@
+zudio =  {'men'   : {'name': 'Vijay',
+                    'address': 'Udaipur',
+                    'product': 'Pants'
+                   },
+         'women': {'name': 'shubham',
+                    'address': 'Ajmer',
+                    }}
+for x,y in zudio.items():
+    print(x,":",)
+    for a,b in y.items():
+        print("\t",a,":",b)
